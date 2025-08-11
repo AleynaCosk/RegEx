5f870c5755b8ea8a0eadfd837e2fc5b1c9855a7e
AleynaCosk <coskunaleyna98@gmail.com>
2025-08-11 14:44:23 +0200

    COMPONENT UPDATE:  Return JSON format output -> FAIL

--- tests/testthat/test-regex.R
+++ tests/testthat/test-regex.R
@@ -8,10 +8,16 @@ describe('Given Character Limitation',{
 
 # Availability
 describe("When character.limitation is called on simple input", {
-  it("Then it returns an integer flag", {
-    out <- character.limitation("abc", 5)
-    out |> expect.integer()  
-    expect_length(out, 1L)
+  it("Then it returns JSON with text and flag", {
+    res <- character.limitation("abc", 5)
+    expect_true(is.character(res)); expect_length(res, 1L)
+
+    j <- jsonlite::fromJSON(res, simplifyVector = FALSE)
+    expect_identical(j$limit, 5L)
+    expect_equal(length(j$results), 1L)
+    expect_identical(j$results[[1]]$text, "abc")
+    expect_true(isTRUE(j$results[[1]]$within))
+    expect_identical(j$results[[1]]$length, 3L)
   })
 })
 
