ae64ac694adbdf90fd3b968f9993fc5aa370ebcc
AleynaCosk <coskunaleyna98@gmail.com>
2025-08-11 09:01:36 +0200

    COMPONENT: Basic returns (type/length) -> FAIL

--- tests/testthat/test-regex.R
+++ tests/testthat/test-regex.R
@@ -5,3 +5,12 @@ describe('Given Character Limitation',{
     character.limitation |> expect.exist()
   })
 })
+
+# Availability
+describe("When character.limitation is called on simple input", {
+  it("Then it returns an integer flag", {
+    out <- character.limitation("abc", 5)
+    out |> expect.integer()  
+    expect_length(out, 1L)
+  })
+})
