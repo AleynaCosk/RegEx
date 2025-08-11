7c029aa83af5affdea081221b36b725433bc1226
AleynaCosk <coskunaleyna98@gmail.com>
2025-08-11 17:06:42 +0200

    UPDATE: Package Name Update to regex.compliance

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(R.Package)
+library(regex.compliance)
 
-test_check("R.Package")
+test_check("regex.compliance")
