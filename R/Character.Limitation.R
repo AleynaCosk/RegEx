a7f96a4f05ab5ef25f3080acb5190f89f0712e17
AleynaCosk <coskunaleyna98@gmail.com>
2025-08-11 09:11:27 +0200

    COMPONENT: Basic returns (type/length) -> TEST

--- R/Character.Limitation.R
+++ R/Character.Limitation.R
@@ -1,3 +1,4 @@
-character.limitation <- \(){
-  
+character.limitation <- \(text, limitation){
+  if (length(text) == 0) return(integer(0))
+  vapply(text, function(t) if (is.na(t)) NA_integer_ else 0L, integer(1))
 }
